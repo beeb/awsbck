be17518f8902fd59c8b560274be99f1a0b467b30
Valentin Bersier <vbersier@gmail.com>
2023-02-08 10:40:42 +0100

    wip: trying to fix the ctrl-c handling

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use anyhow::{anyhow, Context, Result};
 use dotenvy::dotenv;
+use tokio::sync::mpsc;
 use tokio_cron_scheduler::{Job, JobScheduler};
 
 use backup::backup;
@@ -19,6 +20,7 @@ mod prelude;
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    let (tx, mut rx) = mpsc::unbounded_channel();
 
     let params = Arc::new(parse_config().await?);
 
@@ -38,15 +40,15 @@ async fn main() -> Result<()> {
             })?;
             sched.add(job).await?;
             sched.shutdown_on_ctrl_c();
-            sched.set_shutdown_handler(Box::new(|| {
+            sched.set_shutdown_handler(Box::new(move || {
+                let tx = tx.clone();
                 Box::pin(async move {
+                    let _ = tx.send(true);
                     println!("Shut down done");
                 })
             }));
             sched.start().await?;
-            loop {
-                tokio::time::sleep(Duration::from_millis(500)).await;
-            }
+            rx.recv().await;
         }
         None => {
             backup(&params)
