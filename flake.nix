082214220d7c11c072f055fbc96c2aa0654a9c96
beeb <703631+beeb@users.noreply.github.com>
2023-11-28 10:53:14 +0100

    build: update and simplify nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  description = "Flake for development in nix dev shell";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -22,30 +21,19 @@
         };
         lib = pkgs.lib;
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        stdenv = if pkgs.stdenv.isLinux then pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv else pkgs.stdenv;
       in
       {
-        devShells.default = pkgs.mkShell {
-          name = "awsbck";
-          nativeBuildInputs = [
-            pkgs.pkg-config
-            pkgs.clang
-            # Mold Linker for faster builds (only on Linux)
-            (lib.optionals pkgs.stdenv.isLinux pkgs.mold)
-          ];
+        devShells.default = pkgs.mkShell.override { inherit stdenv; } {
           buildInputs = [
-            # We want the unwrapped version, wrapped comes with nixpkgs' toolchain
             pkgs.rust-analyzer-unwrapped
-            # Finally the toolchain
             toolchain
           ];
-          packages = [ ];
 
-          # Environment variables
           RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-          LD_LIBRARY_PATH = lib.makeLibraryPath [ ];
         };
 
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+        packages.default = pkgs.rustPlatform.buildRustPackage.override { inherit stdenv; } {
           pname = "awsbck";
           inherit ((lib.importTOML ./Cargo.toml).package) version;
 
@@ -55,8 +43,6 @@
             lockFile = ./Cargo.lock;
             allowBuiltinFetchGit = true;
           };
-
-          nativeBuildInputs = [ pkgs.installShellFiles ];
 
           buildInputs = lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.Security ];
 
